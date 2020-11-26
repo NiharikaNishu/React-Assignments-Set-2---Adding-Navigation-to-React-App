ab95a4eb20967913ee46431f2f58f4e8308fc8fd
Niharika Nishu <46600925+NiharikaNishu@users.noreply.github.com>
2020-11-26 19:31:38 +0530

    chnge

---
+++ src/components/LocationDisplay.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+export default function LocationDisplay() {
+  let location = useLocation();
+  return <div data-test-id="location-display">{location.pathname}</div>;
+}
